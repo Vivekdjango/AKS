6006df271580f323b4cebd4c53a44f25155ea5c1
Ubuntu <azureuser@workflow-testing.t5fibpebknturbu2k25g5r41fd.gx.internal.cloudapp.net>
2024-05-28 05:54:14 +0000

    workflow testing

---
+++ terraform/main.tf
@@ -0,0 +1,14 @@
+terraform {
+    backend "azurerm" {
+        resource_group_name = "demo"
+        storage_account_name = "terraform1demo"
+        container_name = "terraform"
+        key = "dev.terraform.tfstate"
+    }
+}
+
+module "RG" {
+  source = "./module/RG" 
+  rgname = car.rgname
+  location = var.location
+}
