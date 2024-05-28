3e42b5fc3479f4c4f17b828bef14d8b5ffa8ef38
Ubuntu <azureuser@workflow-testing.t5fibpebknturbu2k25g5r41fd.gx.internal.cloudapp.net>
2024-05-28 08:23:43 +0000

    resolved issue

---
+++ terraform/module/RG/variables.tf
@@ -0,0 +1,9 @@
+variable "rgname" {
+  default = "github-actions1"
+  type    = string
+}
+
+variable "location" {
+  default = "Central US"
+  type    = string
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,9 @@
+variable "rgname" {
+  default = "github-actions-stage"
+  type    = string
+}
+
+variable "location" {
+  default = "Central US"
+  type    = string
+}
