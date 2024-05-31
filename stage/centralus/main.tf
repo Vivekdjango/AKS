7c6329689abafd0255d2b849caa1630d980deb6e
Ubuntu <azureuser@airflow.jhaeitiwrooefpou1yabwndjef.bx.internal.cloudapp.net>
2024-05-31 10:09:24 +0000

    done

--- stage/centralus/main.tf
+++ stage/centralus/main.tf
@@ -1,12 +1,3 @@
-terraform {
-  backend "azurerm" {
-    resource_group_name  = "demo"
-    storage_account_name = "terraform1demo"
-    container_name       = "terraform"
-    key                  = "dev.terraform.tfstate"
-  }
-}
-
 module "RG" {
   source   = "./module/RG"
   rgname   = var.rgname
