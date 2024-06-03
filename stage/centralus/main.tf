c1512e30cc04b6c3b95d34e39875157f5ea8e7b4
Ubuntu <azureuser@terrafromDemo.qh1ytrpppcru1jzxrgxjb0wavf.bx.internal.cloudapp.net>
2024-06-03 05:07:45 +0000

    testing blob state file

--- stage/centralus/main.tf
+++ stage/centralus/main.tf
@@ -1,3 +1,14 @@
+terraform {
+  backend "azurerm" {
+    resource_group_name = "terrafromDemo"
+    storage_account_name = "terraformstatestorage123"
+    container_name = "statefile"
+    key = "dev.terraform.tfstate"
+    
+  }
+}
+
+
 module "RG" {
   source   = "./module/RG"
   rgname   = var.rgname
